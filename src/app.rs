966b7d27efd82b4944443b55051dce6497aa1ad2
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2024-02-19 01:04:28 +0100

    Start implementing `event` and `action` asynchronously
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,13 @@
-use std::io;
-
 use crate::tui;
-use crossterm::event::{self, Event, KeyCode};
+use crossterm::event;
 use ratatui::{
-  prelude::*,
-  widgets::{block::*, *},
+  layout,
+  symbols::{border, line},
+  widgets::{block, block::title, Borders},
 };
+use std::io;
+use std::time;
+use tokio::sync::mpsc;
 
 #[derive(Default)]
 pub struct App {}
@@ -16,8 +18,16 @@ impl App {
   }
 
   pub async fn run(&mut self) -> Result<(), io::Error> {
+    let (action_tx, mut action_rx) = mpsc::unbounded_channel();
     let mut tui = tui::Tui::new()?.mouse(true).paste(true);
     tui.enter()?;
+
+    loop {
+      if let Some(event) = tui.next().await {
+        match event {}
+      }
+    }
+
     let mut size: u16 = 20;
 
     let mut should_quit = false;
@@ -34,11 +44,11 @@ impl App {
 
   fn handle_events_size() -> io::Result<i16> {
     if event::poll(std::time::Duration::from_millis(50))? {
-      if let Event::Key(key) = event::read()? {
+      if let event::Event::Key(key) = event::read()? {
         if key.kind == event::KeyEventKind::Press {
           match key.code {
-            KeyCode::Char('1') => return Ok(1),
-            KeyCode::Char('2') => return Ok(-1),
+            event::KeyCode::Char('1') => return Ok(1),
+            event::KeyCode::Char('2') => return Ok(-1),
             _ => {},
           }
         }
@@ -48,9 +58,9 @@ impl App {
   }
 
   fn handle_events_quit() -> io::Result<bool> {
-    if event::poll(std::time::Duration::from_millis(50))? {
-      if let Event::Key(key) = event::read()? {
-        if key.kind == event::KeyEventKind::Press && key.code == KeyCode::Char('q') {
+    if event::poll(time::Duration::from_millis(50))? {
+      if let event::Event::Key(key) = event::read()? {
+        if key.kind == event::KeyEventKind::Press && key.code == event::KeyCode::Char('q') {
           return Ok(true);
         }
       }
@@ -58,36 +68,37 @@ impl App {
     Ok(false)
   }
 
-  fn ui(size: u16, frame: &mut Frame) {
-    let main_layout =
-      Layout::new(Direction::Vertical, [Constraint::Length(5), Constraint::Min(0), Constraint::Length(5)])
-        .split(frame.size());
-    frame.render_widget(Block::new().borders(Borders::TOP).title("Title Bar"), main_layout[0]);
+  fn ui(size: u16, frame: &mut ratatui::Frame) {
+    let main_layout = layout::Layout::new(
+      layout::Direction::Vertical,
+      [layout::Constraint::Length(5), layout::Constraint::Min(0), layout::Constraint::Length(5)],
+    )
+    .split(frame.size());
+    frame.render_widget(block::Block::new().borders(Borders::TOP).title("Title Bar"), main_layout[0]);
     frame.render_widget(
-      Block::new().borders(Borders::BOTTOM).title(Title::from("Status Bar").position(Position::Bottom)),
+      block::Block::new()
+        .borders(Borders::BOTTOM)
+        .title(title::Title::from("Status Bar").position(title::Position::Bottom)),
       main_layout[2],
     );
 
-    let layout = Layout::default()
-      .direction(Direction::Horizontal)
-      .constraints([Constraint::Percentage(size), Constraint::Percentage(100 - size)])
+    let layout = layout::Layout::default()
+      .direction(layout::Direction::Horizontal)
+      .constraints([layout::Constraint::Percentage(size), layout::Constraint::Percentage(100 - size)])
       .split(main_layout[1]);
 
     frame.render_widget(
-      Block::new()
-        .border_set(symbols::border::PLAIN)
+      block::Block::new()
+        .border_set(border::PLAIN)
         .borders(Borders::TOP | Borders::LEFT | Borders::BOTTOM)
         .title("Left Block"),
       layout[0],
     );
 
-    let top_right_border_set = symbols::border::Set {
-      top_left: symbols::line::NORMAL.horizontal_down,
-      bottom_left: symbols::line::NORMAL.horizontal_up,
-      ..symbols::border::PLAIN
-    };
+    let top_right_border_set =
+      border::Set { top_left: line::NORMAL.horizontal_down, bottom_left: line::NORMAL.horizontal_up, ..border::PLAIN };
     frame.render_widget(
-      Block::new().border_set(top_right_border_set).borders(Borders::ALL).title("Top Right Block"),
+      block::Block::new().border_set(top_right_border_set).borders(Borders::ALL).title("Top Right Block"),
       layout[1],
     );
   }
--- src/tui.rs
+++ src/tui.rs
@@ -1,76 +1,81 @@
-use crossterm::{
-  cursor,
-  event::{DisableMouseCapture, EnableMouseCapture},
-  terminal::{EnterAlternateScreen, LeaveAlternateScreen},
-};
-use std::io::{self, stderr};
-// use tokio::{
-//     sync::{mpsc, Mutex},
-//     task::JoinHandle,
-// };
-
 use crate::utils::unwrap_or_fail;
+use crossterm::{cursor, event, terminal};
+use futures::{FutureExt, StreamExt};
+use ratatui::backend;
+use std::io;
+use tokio::{sync::mpsc, task};
+
+pub enum Event {
+  Init,
+  Quit,
+  Key(event::KeyEvent),
+}
 
 pub struct Tui {
-  pub terminal: ratatui::Terminal<ratatui::backend::CrosstermBackend<std::io::Stderr>>,
+  pub terminal: ratatui::Terminal<backend::CrosstermBackend<std::io::Stderr>>,
+  pub task: task::JoinHandle<()>,
+  pub event_rx: mpsc::UnboundedReceiver<Event>,
+  pub event_tx: mpsc::UnboundedSender<Event>,
   pub mouse: bool,
   pub paste: bool,
 }
 
 impl Tui {
   pub fn new() -> Result<Self, io::Error> {
-    let terminal = unwrap_or_fail(
-      ratatui::Terminal::new(ratatui::backend::CrosstermBackend::new(stderr())),
-      "Failed to create terminal",
-    );
+    let terminal =
+      unwrap_or_fail(ratatui::Terminal::new(backend::CrosstermBackend::new(io::stderr())), "Failed to create terminal");
+    let task = tokio::spawn(async {});
+    let (event_tx, event_rx) = mpsc::unbounded_channel();
     let mouse = false;
     let paste = false;
-    Ok(Self { terminal, mouse, paste })
+    Ok(Self { terminal, task, event_rx, event_tx, mouse, paste })
   }
 
-  pub fn enter(&self) -> Result<(), io::Error> {
-    unwrap_or_fail(crossterm::terminal::enable_raw_mode(), "Failed to enable raw mode");
-    unwrap_or_fail(crossterm::execute!(std::io::stderr(), EnterAlternateScreen, cursor::Hide), "Failed to hide cursor");
+  pub fn enter(&mut self) -> Result<(), io::Error> {
+    unwrap_or_fail(terminal::enable_raw_mode(), "Failed to enable raw mode");
+    unwrap_or_fail(
+      crossterm::execute!(io::stderr(), terminal::EnterAlternateScreen, cursor::Hide),
+      "Failed to hide cursor",
+    );
     if self.mouse {
-      unwrap_or_fail(crossterm::execute!(std::io::stderr(), EnableMouseCapture), "Failed to enable mouse capture");
+      unwrap_or_fail(crossterm::execute!(io::stderr(), event::EnableMouseCapture), "Failed to enable mouse capture");
     }
     if self.paste {
-      unwrap_or_fail(
-        crossterm::execute!(std::io::stderr(), crossterm::event::EnableBracketedPaste),
-        "Failed to enable paste",
-      );
+      unwrap_or_fail(crossterm::execute!(io::stderr(), event::EnableBracketedPaste), "Failed to enable paste");
     }
+    self.start();
     Ok(())
   }
 
   pub fn exit(&self) -> Result<(), io::Error> {
-    unwrap_or_fail(crossterm::execute!(std::io::stderr(), LeaveAlternateScreen, cursor::Show), "Failed to show cursor");
-    unwrap_or_fail(crossterm::terminal::disable_raw_mode(), "Failed to disable raw mode");
+    unwrap_or_fail(
+      crossterm::execute!(io::stderr(), terminal::LeaveAlternateScreen, cursor::Show),
+      "Failed to show cursor",
+    );
+    unwrap_or_fail(terminal::disable_raw_mode(), "Failed to disable raw mode");
     if self.mouse {
-      unwrap_or_fail(crossterm::execute!(std::io::stderr(), DisableMouseCapture), "Failed to disable mouse capture");
+      unwrap_or_fail(crossterm::execute!(io::stderr(), event::DisableMouseCapture), "Failed to disable mouse capture");
     }
     if self.paste {
-      unwrap_or_fail(
-        crossterm::execute!(std::io::stderr(), crossterm::event::DisableBracketedPaste),
-        "Failed to disable paste",
-      );
+      unwrap_or_fail(crossterm::execute!(io::stderr(), event::DisableBracketedPaste), "Failed to disable paste");
     }
     Ok(())
   }
 
-  pub fn suspend(&self) -> Result<(), io::Error> {
+  pub fn suspend(&mut self) -> Result<(), io::Error> {
     self.exit()?;
     #[cfg(not(windows))]
-    unwrap_or_fail(
-      signal_hook::low_level::raise(signal_hook::consts::signal::SIGTSTP),
-      "Failed to leave alternate screen",
-    );
+    unwrap_or_fail(signal_hook::low_level::raise(signal_hook::consts::signal::SIGTSTP), "Failed to raise SIGTSTP");
     Ok(())
   }
 
-  pub fn resume(&self) -> Result<(), io::Error> {
+  pub fn resume(&mut self) -> Result<(), io::Error> {
     self.enter()?;
     Ok(())
+  }
+
+  pub async fn next(&mut self) -> Option<Event> {
+    self.event_rx.recv().await
   }
 
   pub fn mouse(mut self, mouse: bool) -> Self {
@@ -81,5 +86,39 @@ impl Tui {
   pub fn paste(mut self, paste: bool) -> Self {
     self.paste = paste;
     self
+  }
+
+  // ==============================
+  // Private functions
+  // ==============================
+
+  fn start(&mut self) {
+    let _event_tx = self.event_tx.clone();
+    self.task = tokio::spawn(async move {
+      let mut reader = event::EventStream::new();
+      unwrap_or_fail(_event_tx.send(Event::Init), "Failed to send init event");
+      loop {
+        let crossterm_event = reader.next().fuse();
+
+        tokio::select! {
+              maybe_event = crossterm_event => {
+                  match maybe_event {
+                      Some(Ok(event)) => {
+                          match event {
+                              event::Event::Key(key) => {
+                                  if key.kind == event::KeyEventKind::Press {
+                                      unwrap_or_fail(_event_tx.send(Event::Key(key)),
+                                                     format!("Failed to send key event: {:?}", key).as_str());
+                                  }
+                              },
+                              _ => unimplemented!()
+                          }
+                      },
+                      _ => unimplemented!()
+                  }
+              }
+        }
+      }
+    });
   }
 }
