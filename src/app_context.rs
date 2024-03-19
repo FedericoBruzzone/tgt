6201d4c0d3af2155a56e7e3a61e24f91dce4499c
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2024-03-19 01:05:15 +0100

    Add documentation for `app_context`
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- src/app_context.rs
+++ src/app_context.rs
@@ -53,54 +53,106 @@ impl AppContext {
             paste,
         })
     }
-
+    /// Set the frame rate at which the user interface should be rendered.
+    ///
+    /// # Arguments
+    /// * `frame_rate` - The frame rate at which the user interface should be
+    ///
+    /// # Returns
+    /// * `Self` - The `App` struct.
     pub fn with_frame_rate(mut self, frame_rate: f64) -> Self {
         self.frame_rate = frame_rate;
         self
     }
-
+    /// Set the boolean flag that represents whether the mouse is enabled or
+    /// not.
+    ///
+    /// # Arguments
+    /// * `mouse` - A boolean flag that represents whether the mouse is enabled
+    ///  or not.
+    ///
+    ///  # Returns
+    ///  * `Self` - The `App` struct.
     pub fn with_mouse(mut self, mouse: bool) -> Self {
         self.mouse = mouse;
         self
     }
-
+    /// Set the boolean flag that represents whether the clipboard is enabled
+    /// or not.
+    ///
+    /// # Arguments
+    /// * `paste` - A boolean flag that represents whether the clipboard is
+    ///  enabled or not.
+    ///
+    ///  # Returns
+    ///  * `Self` - The `App` struct.
     pub fn with_paste(mut self, paste: bool) -> Self {
         self.paste = paste;
         self
     }
-
+    /// Get the user interface for the application.
+    ///
+    /// # Returns
+    /// * `&Tui` - A reference to the user interface for the application.
     pub fn tui_ref(&self) -> &Tui {
         &self.tui
     }
-
+    /// Get the user interface for the application.
+    ///
+    /// # Returns
+    /// * `&mut Tui` - A mutable reference to the user interface for the
+    ///  application.
     pub fn tui_mut_ref(&mut self) -> &mut Tui {
         &mut self.tui
     }
-
+    /// Get the keymap configuration.
+    ///
+    /// # Returns
+    /// * `&KeymapConfig` - A reference to the keymap configuration.
     pub fn keymap_config_ref(&self) -> &KeymapConfig {
         &self.keymap_config
     }
-
+    /// Get the keymap configuration.
+    ///
+    /// # Returns
+    /// * `&mut KeymapConfig` - A mutable reference to the keymap configuration.
     pub fn keymap_config_mut_ref(&mut self) -> &mut KeymapConfig {
         &mut self.keymap_config
     }
-
+    /// Get the unbounded receiver that receives action for processing.
+    ///
+    /// # Returns
+    /// * `&UnboundedReceiver<Action>` - A reference to the unbounded receiver
+    /// that receives action for processing.
     pub fn action_rx_ref(&self) -> &UnboundedReceiver<Action> {
         &self.action_rx
     }
-
+    /// Get the unbounded receiver that receives action for processing.
+    ///
+    /// # Returns
+    /// * `&mut UnboundedReceiver<Action>` - A mutable reference to the unbounded receiver that receives action for processing.
     pub fn action_rx_mut_ref(&mut self) -> &mut UnboundedReceiver<Action> {
         &mut self.action_rx
     }
-
+    /// Get the unbounded sender that send action for processing.
+    ///
+    /// # Returns
+    /// * `&UnboundedSender<Action>` - A reference to the unbounded sender that
     pub fn action_tx_ref(&self) -> &UnboundedSender<Action> {
         &self.action_tx
     }
-
+    /// Get the unbounded sender that send action for processing.
+    ///
+    /// # Returns
+    /// * `&mut UnboundedSender<Action>` - A mutable reference to the unbounded
     pub fn action_tx_mut_ref(&mut self) -> &mut UnboundedSender<Action> {
         &mut self.action_tx
     }
-
+    /// Get a clone of the unbounded sender that send action for processing.
+    /// This is useful for sending action from other components.
+    ///
+    /// # Returns
+    /// * `UnboundedSender<Action>` - A clone of the unbounded sender that send
     pub fn action_tx_clone(&self) -> UnboundedSender<Action> {
         self.action_tx.clone()
     }
